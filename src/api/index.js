36494ab3033b3158bfbd513b8217f85c43051e45
pd-andy <andrew.thompson@qmul.ac.uk>
2019-02-13 19:29:24 +0000

    :bulb: Document src/api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@ const api = {
   v1: require('./v1/index.js')
 }
 
+// Programmatically determine what version of api to expose.
+// This is necessary because expressions, including string 
+// literals cannot be used in a require() call.
 module.exports = (version => {
   switch (version) {
     case 'test':
@@ -11,6 +14,9 @@ module.exports = (version => {
     case 'v1':
       console.log('loading v1 api')
       return api.v1
+    // Fall back to the test api if no version, or an invalid
+    // version is supplied. This should probably default to v1
+    // once in production.
     default:
       return api.test
   }
